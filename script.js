026aa09eb660ea47589c3cf0c2df4a561858e08d
Mateus V. Gomes <monkmitogames@hotmail.com>
2023-02-27 18:44:28 +0000

    Cifra implementada mas não é aprova de absurdos

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ let chave;
 
 traduzirRadio.addEventListener('change', function () {
 
-    
+
 
     if (traduzirRadio.checked == true) {
 
-        sentenca = 'traduiz';
+        sentenca = 'traduzir';
 
-        alert(sentenca);
+  
 
     }
 
@@ -33,7 +33,6 @@ escreverRadio.addEventListener('change', function () {
 
         sentenca = 'escrever';
 
-        alert(sentenca);
 
     }
 
@@ -41,20 +40,94 @@ escreverRadio.addEventListener('change', function () {
 
 chaveInput.addEventListener('change', function () {
 
- chave = chaveInput.value;
+    chave = chaveInput.value;
+
 
 });
 translateButton.addEventListener('click', () => {
-    console.log(chave); 
-    let cifrinha=cifraInput.value;
-    prepareCipher(sentenca,chave,cifrinha);
+    console.log(chave);
+    let cifrinha = cifraInput.value;
+    prepareCipher(sentenca, chave, cifrinha);
 
 })
 
 
-function prepareCipher(sentenca,chave,cifra) {
-  sentenca===undefined?alert("Você precisa marcar se quer traduzir ou escrever"):
-  chave===undefined?alert("você precisa escrever a chave"):
-  cifraInput.value===''?alert('Você precisa digitar a cifra'):alert('ta')
+function prepareCipher(sentenca, chave, cifra) {
+     chave=+chave;
+    sentenca === undefined ? alert("Você precisa marcar se quer traduzir ou escrever") :
+        chave === undefined ? alert("você precisa escrever a chave") :
+            cifraInput.value === '' ? alert('Você precisa digitar a cifra') : sentenca === 'traduzir' ? translateCipher(chave, cifra) : writeCipher(chave, cifra);
+
+}
+
+function translateCipher(chave, cifra) {
+     
+    cifra = cifra.toUpperCase();
+ 
+    let translated = "", actual;
+
+    for (let i = 0; i < cifra.length; i++) {
+        
+        actual = convertLetterToAsc(cifra.charAt(i));
+          
+        if (actual - chave < 65) {
+
+            if (actual >= 33 && actual <= 64) {
+
+                translated += convertAscToLetter(actual);
+
+            }
+            else {
+                translated += convertAscToLetter(actual + chave);
+            }
+        }
+        else {
+
+            translated += convertAscToLetter(actual - chave);
+
+        }
+    }
+
+    return alert(translated.replaceAll("-", " "));
+
+}
+
+function writeCipher(chave, cifra) {
+  
+    cifra = cifra.toUpperCase();
+    console.log(cifra);
+    let translated = "", actual;
+
+    for (let i = 0; i < cifra.length; i++) {
+        
+        actual = convertLetterToAsc(cifra.charAt(i));
+
+        if (actual + chave > 90) {
+
+            if (actual > 90) {      
+                translated += convertAscToLetter(actual);
+                console.log(translated);
+            }
+            else {
+                 console.log('aqui')
+                translated += convertAscToLetter(actual - chave);  
+            }
+        }
+        else {          
+            translated += convertAscToLetter(actual + chave);
+        }
+    }
+    return alert(translated.replaceAll("-", " "));
+
+}
+
+function convertLetterToAsc(letter) {
+
+    return letter.charCodeAt(0)
+
+}
+function convertAscToLetter(asc) {
+
+    return String.fromCharCode(asc);
 
 }
